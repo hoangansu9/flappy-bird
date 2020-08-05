b71d411fccd77b2da97ab2d6455ed3e352f97d5e
an <hoangansu9@gmail.com>
2020-08-05 11:34:46 +0700

    Updates

--- src/component/Foreground/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './style.scss';
-const Foreground = () => {
-  return <div className='foreground-wrapper'></div>;
-};
-
-export default Foreground;
--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore, applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './rerducers';
-export default () => {
-  const store = createStore(rootReducer, applyMiddleware(thunk));
-  return store;
-};
